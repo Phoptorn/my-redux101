2b9cfa45a7cd2b3b3a79d1c7babaaf2ef5dd4cbf
Phoptorn Khowhoudchai <phoptorn.khow@gmail.com>
2023-02-25 14:19:58 +0700

    first commit

--- src_old/reducers/itReducers.tsx
+++
@@ -1,5 +0,0 @@
-const it = "ทดสอบข้อความ itReducer"
-
-export const itReducer = (state = it) => {
-	return state
-}
--- src_old/reducers/userReducers.tsx
+++
@@ -1,5 +0,0 @@
-const user = "ทดสอบข้อความ userReducer"
-
-export const userReducer = (state = user) => {
-		return state
-}
